f845ea4ac1d252ed72e3bae1a5bacf7baf44d492
saudaihuy <40286107+saudaihuy@users.noreply.github.com>
2018-10-13 19:43:11 +0700

    Update

---
+++ Quan Ly Ve So/DTO/AgencyDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Quan_Ly_Ve_So.DTO
+{
+    public class AgencyDTO
+    {
+        public string ID_AGENCY { get; set; }
+        public string NAME_AGENCY { get; set; }
+        public string ADDR { get; set; }
+        public string NUMPHONE { get; set; }
+        public int ACTIVATE { get; set; }
+    }
+}
